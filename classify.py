3ebdeca6a5f07c94e8e15d9532b37a464c89dbc9
Charlie You <charlie@charlieyou.me>
2016-11-29 11:48:34 -0500

    everything sans time series

--- classify.py
+++ classify.py
@@ -5,15 +5,29 @@ import pandas as pd
 
 
 def tweets(clf):
-    pass
-
+    clinton, trump = pickle.load(open('tweets_preprocessed', 'rb'))
+    c_result = clf.predict([' '.join(doc) for _, doc in clinton])
+    t_result = clf.predict([' '.join(doc) for _, doc in trump])
+    df = pd.DataFrame([[c_result.sum(), len(c_result)],
+        [t_result.sum(), len(t_result)]],
+        index=('Clinton', 'Trump'), columns=('Positive', 'Total'))
+    df.to_csv('twitter_results.csv')
 
 def debates(clf):
-    pass
+    names = ('clinton1', 'clinton2', 'clinton3', 'trump1', 'trump2', 'trump3')
+    results = []
+    for name, obj in zip(names, pickle.load(open('debates_preprocessed', 'rb'))):
+        temp = [x for x in obj if x != []]
+        results.append(clf.predict([' '.join(doc) for doc in temp]))
 
+    for i, result in enumerate(results):
+        results[i] = (result.sum(), len(result), float(result.sum()) / len(result))
+
+    df = pd.DataFrame(results, index=names, columns=('positive', 'total', 'percent'))
+    df.to_csv('debate_results.csv')
 
 if __name__ == '__main__':
     clf = joblib.load('clf/clf.p')
 
     tweets(clf)
-    debates(clf)
+    # debates(clf)
--- train.py
+++ train.py
@@ -1,18 +1,20 @@
 import cPickle as pickle
 import joblib
 
+from sklearn.cross_validation import cross_val_score
 from sklearn.pipeline import Pipeline
-from sklearn.feature_extraction.text import TfidfVectorizer as Vectorizer
+from sklearn.feature_extraction.text import CountVectorizer as Vectorizer
 from sklearn.naive_bayes import MultinomialNB as NB
 
 
 if __name__ == '__main__':
     raw, y = pickle.load(open('training_data.p', 'rb'))
-    X = [''.join(doc) for doc in raw]
+    X = [' '.join(doc) for doc in raw]
 
-    vec = Vectorizer(ngram_range=(1, 2))
+    vec = Vectorizer(ngram_range=(1, 1))
     clf = NB()
     vec_clf = Pipeline([('vectorizer', vec), ('clf', clf)])
+    print cross_val_score(vec_clf, X, y)
 
     vec_clf.fit(X, y)
     joblib.dump(vec_clf, 'clf/clf.p')
