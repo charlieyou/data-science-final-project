7968261c3e44fe3544360a08fea271b611d105c1
Charlie You <charlie@charlieyou.me>
2016-11-29 12:59:55 -0500

    final commit

--- classify.py
+++ classify.py
@@ -13,6 +13,31 @@ def tweets(clf):
         index=('Clinton', 'Trump'), columns=('Positive', 'Total'))
     df.to_csv('twitter_results.csv')
 
+
+def time_series(clf):
+    clinton, trump = pickle.load(open('tweets_preprocessed', 'rb'))
+
+    c_dates = [date for date, _ in clinton]
+    c_result = clf.predict([' '.join(doc) for _, doc in clinton])
+
+    t_dates = [date for date, _ in trump]
+    t_result = clf.predict([' '.join(doc) for _, doc in trump])
+
+    c = pd.Series(c_result, index=c_dates)
+    t = pd.Series(t_result, index=t_dates)
+
+    c = c.groupby(pd.TimeGrouper(freq='D')).mean()
+    t = t.groupby(pd.TimeGrouper(freq='D')).mean()
+
+    c = pd.rolling_mean(c, 14)
+    t = pd.rolling_mean(t, 14)
+
+    df = pd.concat([c, t], axis=1)
+    df.columns = ('Clinton', 'Trump')
+
+    df.to_pickle('time_series.p')
+
+
 def debates(clf):
     names = ('clinton1', 'clinton2', 'clinton3', 'trump1', 'trump2', 'trump3')
     results = []
@@ -29,5 +54,6 @@ def debates(clf):
 if __name__ == '__main__':
     clf = joblib.load('clf/clf.p')
 
-    tweets(clf)
+    time_series(clf)
+    # tweets(clf)
     # debates(clf)
